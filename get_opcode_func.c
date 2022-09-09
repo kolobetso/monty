0c76649254ef5d59c3f9ec40beab4e805cef42ec
George Malepeng <jaquar93@gmail.com>
2022-09-09 12:03:18 -0700

    Task updates

---
+++ get_opcode_func.c
@@ -0,0 +1,36 @@
+#include "monty.h"
+
+/**
+ * get_opcode_func - function to select correct operation function
+ * @token1: 1st bytecode input (opcode)
+ * Return: pointer to correct operation function
+ */
+void (*get_opcode_func(char *token1))(stack_t **stack, unsigned int line_number)
+{
+	instruction_t instruction_s[] = {
+		{"pall", pall},
+		{"pint", pint},
+		{"pop", pop},
+		{"swap", swap},
+		{"add", _add},
+		{"sub", _sub},
+		{"mul", _mul},
+		{"div", _div},
+		{"mod", _mod},
+		{"pchar", pchar},
+		{"pstr", pstr},
+		{"nop", nop},
+		{"rotl", rotl},
+		{"rotr", rotr},
+		{NULL, NULL}
+	};
+	int i = 0;
+
+	while (instruction_s[i].f != NULL)
+	{
+		if (strcmp(token1, instruction_s[i].opcode) == 0)
+			return (instruction_s[i].f);
+		i++;
+	}
+	return (NULL);
+}
